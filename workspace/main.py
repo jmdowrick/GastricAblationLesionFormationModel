069daa640f5cf5cfbb12b62245ec6e1e0f7b7347
jmdowrick <146781982+jmdowrick@users.noreply.github.com>
2026-07-24 09:18:07 +1200

    Add parameter yaml

--- workspace/main.py
+++ workspace/main.py
@@ -1,1 +1,8 @@
+import ufl
+import numpy
+from mpi4py import MPI
+from dolfinx import mesh, fem
+from dolfinx.fem.petsc import NonlinearProblem
+from src.parameters import load_parameters
 
+params = load_parameters('parameters.yml')
